5c9370131e69b498678c4124453f5e01cbac7ce4
kimSR0916 <143482628+kimSR0916@users.noreply.github.com>
2024-11-29 18:43:52 +0900

    kimSR0916 first commit

---
+++ src/main/java/controller/member/DeleteMemberController.java
@@ -0,0 +1,30 @@
+package controller.member;
+
+import manager.MemberManager;
+import controller.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteMemberController implements Controller {
+    private MemberManager memberManager;
+
+    public DeleteMemberController() {
+        this.memberManager = new MemberManager();
+    }
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String memberId = request.getParameter("memberId");
+
+        boolean success = memberManager.deleteMember(Integer.parseInt(memberId));
+
+        if (success) {
+            request.setAttribute("message", "사용자 삭제 성공");
+            return "/member/deleteSuccess.jsp"; // Forwarding
+        } else {
+            request.setAttribute("error", "사용자 삭제 실패");
+            return "/member/deleteFail.jsp"; // Forwarding
+        }
+    }
+}
